080c98b348bca91b777179c88582966e273ac6f4
divyangkhatri <divyangkhatri.dk@gmail.com>
2022-11-23 15:10:06 +0530

    - Implemented firebase setup

--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, {FormEvent, useCallback, useState} from 'react'
+import { signInWithEmailAndPassword ,createUserWithEmailAndPassword} from 'firebase/auth'
 import Image from 'next/image'
 import styles from '../../styles/Login.module.scss'
 import LoginCover from '../../public/images/LoginCover.png'
@@ -9,6 +10,7 @@ import {
     Grid,
     Link
 } from '@mui/material';
+import {auth} from "../firebase";
 
 
 export default function Login() {
@@ -24,14 +26,58 @@ export default function Login() {
             }
         });
     }
-    const handleSubmit = () => {
 
+    const onGetToken = useCallback(async ()=>{
+        const token = await auth?.currentUser?.getIdToken(true)
+        if(token){
+            console.log("Tokjenss",token)
+        }else{
+            alert("Not tokem found")
+        }
+    },[])
+
+    const handleSubmit = (e:FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        signInWithEmailAndPassword(auth,fieldValues.email,fieldValues.password).then((user)=>{
+            console.log("Userss",user)
+        }).catch((error:any)=> {
+            console.error("Error in sign in",error.message)
+            if(error.code === "auth/user-not-found"){
+                createUserWithEmailAndPassword(auth,fieldValues.email,fieldValues.password).then(user=>{
+                    console.log("Userss",user)
+                }).catch((err:any)=>{
+                    console.error("error in create user",err)
+                })
+            }
+        })
+        console.log("Field value",fieldValues)
     }
     return (
         <div className={styles.logincontainer}>
             <div className={styles.rightsection}>
                 <Image src={Logo} alt={''} />
                 <p className={styles.title}>You are hopping to the</p>
+                <Grid item>
+                    <Button
+                        variant="contained"
+                        type="submit"
+                        className="button-block"
+                        onClick={onGetToken}
+                        sx={{
+                            background: '#FFFFFF',
+                            border: '1px solid #000000',
+                            width: '450px',
+                            boxShadow: '4px 4px 0px #70FFC3',
+                            borderRadius: '4px',
+                            color: '#000',
+                            '&:hover': {
+                                backgroundColor: '#70FFC3',
+                            },
+                        }}
+                    >
+                        GET TOKEN
+                    </Button>
+                </Grid>
                 <span className={styles.transformdivwrraper}><p className={styles.transformText}>right place</p></span>
                 <div className={styles.loginform}>
                     <form onSubmit={handleSubmit}>
---
+++ components/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD9ggYMbQaqO9-nryt-COUIgKWT8Wf8p7g",
+    authDomain: "hop3-660b9.firebaseapp.com",
+    projectId: "hop3-660b9",
+    storageBucket: "hop3-660b9.appspot.com",
+    messagingSenderId: "567493302718",
+    appId: "1:567493302718:web:39ccd3bd2955f8429a81ef",
+    measurementId: "G-9DYDDHW0FM"
+};
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
