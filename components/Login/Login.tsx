89d1377ea3d5d548664d14c34d68778080799255
jigarc007 <jigarv007@gmail.com>
2022-11-23 13:32:52 +0530

    change button text

--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -82,7 +82,7 @@ export default function Login() {
                                           },
                                     }}
                                 >
-                                    Submit
+                                    Login
                                 </Button>
                             </Grid>
                         </Grid>
