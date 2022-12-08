e640a33d1620eb4059b054490a3a931b37ff5618
jigarc007 <jigarv007@gmail.com>
2022-12-09 01:04:53 +0530

    update the test cases

--- tests/Login.test.tsx
+++ tests/Login.test.tsx
@@ -9,25 +9,12 @@ jest.mock('next/router', () => ({
     useRouter: jest.fn(),
   }));
 describe('login',()=>{
-    it('check elments render or not', () => {
+    it('check elments render or not and its event', () => {
         render(<Login />);
+        const button = screen.getByTestId('login')
+        expect(button).toBeInTheDocument();
+        expect(button).toHaveAttribute('type','submit');
+        expect(fireEvent.click(button)).toBeTruthy()
 
-        expect(screen.getByTestId('email')).toBeInTheDocument();
-        expect(screen.getByTestId('email')).toHaveAttribute('type','email');
-
-        expect(screen.getByTestId('password')).toBeInTheDocument();
-        expect(screen.getByTestId('password')).toHaveAttribute('type','password');
-
-        expect(screen.getByTestId('login')).toBeInTheDocument();
-
-    })
-    it('check the element change event', () => {
-        render(<Login />);
-        const email= screen.getByTestId('email');
-        fireEvent.change(email, { target: {value: 'demo@gmail.com'}})
-        expect(email.value).toBe('demo@gmail.com')
-        const password= screen.getByTestId('password');
-        fireEvent.change(password, { target: {value: 'demo@gmail.com'}})
-        expect(password.value).toBe('demo@gmail.com')
     })
 })
