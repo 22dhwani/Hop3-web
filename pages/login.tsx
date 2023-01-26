edfe848bce22ad502d100b0fd02f463b68daeb4a
David Zou <davidzsy20@gmail.com>
2023-01-26 14:32:12 -0800

    add airtable

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Login from '../components/Login/Login';
 
+import { baseShopCMS } from '../services/airtable';
+
 const LoginPage = () => {
+  useEffect(() => {
+    baseShopCMS('Table 1')
+      .select({
+        // Select the first 3 records in Grid view:
+        maxRecords: 3,
+        view: 'Grid view',
+      })
+      .eachPage(function page(records, fetchNextPage) {
+        // This function (`page`) will get called for each page of records.
+
+        records.forEach(function (record) {
+          console.log('Retrieved', record.get('Product Name'));
+        });
+      });
+  }, []);
+
   return <Login />;
 };
 
---
+++ services/airtable.ts
@@ -0,0 +1,10 @@
+import Airtable from 'airtable';
+
+const personalAccessToken =
+  'patoegYXDw3iAuK6h.be8c0fb469e9db1f64007f1e6c461c16f332233f0e3b8123867d5c9b795ce082';
+
+Airtable.configure({ apiKey: personalAccessToken });
+
+export const baseShopCMS = Airtable.base('app6ldGCFVSkP9wtG');
+
+export default Airtable;
